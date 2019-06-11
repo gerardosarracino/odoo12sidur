5fb339e8d96493979802a9b344681b8f68b1945e
gerardosarracino <gerardo.sarracino.s@gmail.com>
2019-06-11 13:54:48 -0700

    se agrego modelo de plantillas, ver detalles

---
+++ models/Plantillas.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields
+
+
+class Plantilla(models.Model):
+    _name = 'platillas.plantilla'
+
+    select_documento = [('1', 'INVITACIONES'), ('2', 'BASES'), ('3', 'VISITA A LA OBRA'), ('4', 'JUNTA ACLARACIONES'),
+                        ('5', 'APERTURA DE PROPUESTAS'), ('6', 'FALLO'), ('7', 'CONTRATOS'), ('8', 'CONVENIOS'),
+                        ('9', 'VERIFICACIÓN TERMINACIÓN'), ('10', 'RECEPCIÓN DE OBRA'), ('11', 'FINIQUITO'),
+                        ('12', 'CIERRE ADMVO.'), ('13', 'EXT. DER. Y OBLIG'), ('14', 'TODOS')
+                        ]
+    select_normatividad = [('1', 'FEDERAL'), ('2', 'ESTATAL/LOCAL'), ('3', 'TODOS')]
+    select_periodo = [('1', 'ANUAL'), ('2', 'MULTIANUAL'), ('3', 'TODOS')]
+    select_anticipo = [('1', 'SI'), ('2', 'NO'), ('3', 'TODOS')]
+    select_tipo_procedimiento = [('1', 'ADJUDICACIÓN DIRECTA'), ('2', 'ADJUDICACIÓN POR EXCEPCIÓN O EMERGENCIA'),
+                                 ('3', 'INVITACIÓN RESTRINGIDA'), ('4', 'LICITACION NACIONAL'), ('5', 'TODOS')]
+    # -----
+    tipo_documento = fields.Selection(select_documento, string="Tipo Documento:", required=True)
+    normatividad = fields.Selection(select_normatividad, string="Normatividad:", required=True)
+    # los datos de programa se podrian heredar, pendiente
+    programa = fields.Selection(string="Programa:", required=True)
+    # - - - - -
+    periodo = fields.Selection(select_periodo, string="Periodo:", required=True)
+    anticipo = fields.Selection(select_anticipo, string="Anticipo:", required=True)
+    tipo_procedimiento = fields.Selection(select_tipo_procedimiento, string="Tipo de Procedimiento:", required=True)
+    # subir archivo field
+    nombre_documento = fields.Char(string="Nombre Documento:", required=True, )
