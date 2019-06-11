f4311ffe8b83f71507b119339620487712a12a03
gerardosarracino <gerardo.sarracino.s@gmail.com>
2019-06-11 14:13:38 -0700

    se agrego control_expediente, ver detalles tambien aqui

---
+++ models/Control_expediente.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields
+
+
+class ExpedienteUnico(models.Model):
+    _name = 'control_expediente.expediente_unico'
+
+    select_etapa = [('1', 'PREVIO AL PROCESO DE CONTRATACIÓN'), ('2', 'DURANTE EL PROCESO DE CONTRATACIÓN'),
+                    ('3', 'DESPUÉS DEL PROCEDIMIENTO DE CONTRATACIÓN Y ANTES DE EJECUTAR LA OBRA'),
+                    ('4', 'DURANTE LA EJECUCIÓN DE LA OBRA'), ('5', 'DESPUÉS DE LA EJECUCIÓN DE LA OBRA')]
+    etapa = fields.Selection(select_etapa, string="Etapa:", required=True)
+    # responsable = fields.Selection(string="Responsable:", required=True) pendiente, usuarios o que pex
+    nombre = fields.Char(string="Nombre:", required=True)
+    orden = fields.Char(string="Orden:", required=True)
