e15574bce706f557e37af5d52945f27b5bbd9a92
gerardosarracino <gerardo.sarracino.s@gmail.com>
2019-06-19 21:59:34 -0700

    vistas firmas_logos unificada

--- models/Firmas_logos.py
+++ models/Firmas_logos.py
@@ -3,13 +3,6 @@
 from odoo import models, fields, api
 
 
-class Municipio(models.Model):
-    _name = 'firmas_logos.municipio'
-
-    select = [('sidur', 'SIDUR')]
-    name = fields.Selection(select, string="Municipio:", required=True)
-
-
 class FirmasLogos(models.Model):
     _name = 'firmas_logos.firmas_logos'
     # MUNICIPIO ?
@@ -56,71 +49,3 @@ class FirmasLogos(models.Model):
     nombre_testigo_tres = fields.Char(string="Nombre testigo tres:", required=True)
     puesto_testigo_tres = fields.Char(string="Puesto testigo tres:", required=True)
 
-class Titular(models.Model):
-    _name = 'firmas_logos.titular'
-
-    nombre_titular = fields.Char(string="Nombre:", required=True)
-    puesto = fields.Char(string="Puesto:", required=True)
-    fecha_nombramiento = fields.Date(string="Fecha de Nombramiento:", required=True)
-# contraloria interna no aparece en el cronograma de actividades pero en sidur si, PENDIENTE
-
-
-class ContraloriaInterna(models.Model):
-    _name = 'firmas_logos.contraloria_interna'
-
-    name = fields.Char(string="Representante:", required=True)
-    puesto = fields.Char(string="Puesto:", required=True)
-    fecha_nombramiento = fields.Date(string="Fecha Nombramiento:", required=True)
-
-
-class RepresentanteCiudadano(models.Model):
-    _name = 'firmas_logos.representante_ciudadano'
-
-    name = fields.Char(string="Nombre:", required=True)
-    puesto = fields.Char(string="Puesto:", required=True)
-
-
-class DirectorCoordinadorAdministrativo(models.Model):
-    _name = 'firmas_logos.director_coordinador_administrativo'
-
-    name = fields.Char(string="Nombre:", required=True)
-    puesto = fields.Char(string="Puesto:", required=True)
-
-
-class DirectorCoordinadorPlaneacion(models.Model):
-    _name = 'firmas_logos.director_coordinador_planeacion'
-
-    name = fields.Char(string="Nombre:", required=True)
-    puesto = fields.Char(string="Puesto:", required=True)
-
-
-class DirectorCoordinadorJuridico(models.Model):
-    _name = 'firmas_logos.director_coordinador_juridico'
-
-    name = fields.Char(string="Nombre:", required=True)
-    puesto = fields.Char(string="Puesto:", required=True)
-
-
-class ResponsableSubdirectorObra(models.Model):
-    _name = 'firmas_logos.responsable_subdirector_obra'
-
-    name = fields.Char(string="Nombre:", required=True)
-    puesto = fields.Char(string="Puesto:", required=True)
-
-
-class ResponsableLicitaciones(models.Model):
-    _name = 'firmas_logos.responsable_licitaciones'
-
-    name = fields.Char(string="Nombre:", required=True)
-    puesto = fields.Char(string="Puesto:", required=True)
-
-
-class FirmasContratos(models.Model):
-    _name = 'firmas_logos.firmas_contratos'
-
-    name = fields.Char(string="Nombre testigo uno:", required=True)
-    puesto_testigo_uno = fields.Char(string="Puesto testigo uno:", required=True)
-    nombre_testigo_dos = fields.Char(string="Nombre testigo dos:", required=True)
-    puesto_testigo_dos = fields.Char(string="Puesto testigo dos:", required=True)
-    nombre_testigo_tres = fields.Char(string="Nombre testigo tres:", required=True)
-    puesto_testigo_tres = fields.Char(string="Puesto testigo tres:", required=True)
